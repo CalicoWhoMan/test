a3372fedc3192512d278cbfad49761afcd6f9618
calicowhoman <mikeyshrier@gmail.com>
2021-08-02 09:46:22 -0700

    testies

---
+++ holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/** 
+ * struct func - a structure containing chars to compare with modifiers and 
+ * then decide which is the right function to use
+ * @dec: char that we want to decide to
+ * @  :
+ */
+
+typedef struct aim
+{
+/* char aim is the operation */
+char aim
+/*int define is the place where the function is pointing to that
+ * we want to return to if it matches */
+int (*define)(va_list);
+} aim_f;
+
+
---
+++ printf.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * printf - This is the string that contains the text to be written
+ * to stdout with different types of arguments.
+ * @format: the string that shows us what is about to be printed
+ * Return: the number of characters successfully printed by the function
+ */
+
+int _printf(const char *format, ...);
+
+{
+
+
+
+
+
+
+
+
+
+}
