d6f2ae9683e1331054a03665acbb3aca2265f21d
calicowhoman <mikeyshrier@gmail.com>
2021-08-03 07:23:33 -0700

    trek

--- printf.c
+++ printf.c
@@ -11,13 +11,34 @@
 int _printf(const char *format, ...);
 
 {
+int a, inc;
+va_list op_list;
+int (*f)(va_list);
 
+if (format == NULL)
+return (-1);
 
+va_start(op_list, format);
+a = inc = 0;
 
-
-
-
-
-
-
+while (format[a] != '\0')
+{
+if (format[a] == '%')
+{
+if (format[a +1] == '\0')
+return (-1);
+f = get_func(format[a + 1]);
+if (f == NULL)
+count += print_nan(format[a], format[a + 1]);
+else
+inc += f(op_list);
+a++;
+}
+else
+{
+putchar(format[a]);
+inc++;
+}
+va_end(op_list);
+return(inc);
 }
