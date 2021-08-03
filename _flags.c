7d20d3624ebc67dc70755ad46b31d999657c5d1b
calicowhoman <mikeyshrier@gmail.com>
2021-08-03 08:00:30 -0700

    flag converter for s, d, i, c

--- _flags.c
+++ _flags.c
@@ -2,15 +2,29 @@
 #include <stdlib>
 
 /**
- *
- *
- *
- *
- *
+ * get_flags - flag option output for printf
+ * @format: converter
+ * Return: pointer for the function otehrwise NULL
  */
 
 int (*get_flags(const char *format))(va_list)
 
 {
 unsigned int a;
+print_z p[] = {
+{"c". print_c},
+{"s". print_c},
+{"d". print_c},
+{"i". print_c},
+{NULL, NULL}
+};
 
+for (a = 0 ; p[a].z =! NULL; a++)
+{
+	if (*(p[a].z) == *format)
+	{
+		break;
+	}
+}
+return (p[a].f);
+}
